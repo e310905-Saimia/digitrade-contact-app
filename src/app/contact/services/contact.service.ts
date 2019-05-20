32947602daf6bfd34f341cd1423f6a0925bf100a
e310905-saimia <jouni.k.kononen@gmail.com>
2019-05-21 00:05:13 +0300

    contact.service.ts updated

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Contact} from '../contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  private contacts: Contact[];
 
-  constructor() { }
+  constructor() {
+    this.contacts = [
+      new Contact('Aku', 'Ankka', '313'),
+      new Contact('Roope', 'Ankka', '999'),
+      new Contact('Taavi', 'Ankka', '000')
+    ];
+
+    this.contacts.push(new Contact('Hannu', 'Hanhi', '123123'));
+  }
+
+  get(): Contact[] {
+    return this.contacts;
+  }
 }
