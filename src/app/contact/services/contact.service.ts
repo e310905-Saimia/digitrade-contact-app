cc157f28f25914011eaf23b26103591f1229b00e
e310905-saimia <jouni.k.kononen@gmail.com>
2019-05-21 02:03:53 +0300

    First version of contact-http.service

--- src/app/contact/services/contact.service.ts
+++ src/app/contact/services/contact.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Contact} from '../contact';
+import {ContactHttpService} from './contact-http.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +8,20 @@ import {Contact} from '../contact';
 export class ContactService {
   private contacts: Contact[];
 
-  constructor() {
-    this.contacts = [
+  constructor(private contactHttpService: ContactHttpService) {
+    /* this.contacts = [
       new Contact('Aku', 'Ankka', '313'),
       new Contact('Roope', 'Ankka', '999'),
       new Contact('Taavi', 'Ankka', '000')
     ];
 
     this.contacts.push(new Contact('Hannu', 'Hanhi', '123123'));
+     */
   }
 
   get(): Contact[] {
-    return this.contacts;
+    // return this.contacts;
+    console.log(this.contactHttpService.get());
+    return [];
   }
 }
