05b022e520791b14688b85bc909bf3a2a2a79304
e310905-saimia <jouni.k.kononen@gmail.com>
2019-05-21 10:05:33 +0300

    Observable done

--- src/app/contact/services/contact-http.service.ts
+++ src/app/contact/services/contact-http.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Contact} from '../contact';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {environment} from '../../../environments/environment';
 
 
 @Injectable({
@@ -10,14 +13,16 @@ export class ContactHttpService {
   private url: string;
 
   constructor(private http: HttpClient) {
-    this.url = 'http://localhost:3000/contacts';
+    // this.url = 'http://localhost:3000/contacts';
+    this.url = environment.endpointUrl + '/contacts';
   }
 
-  get() {
-    return this.http
-      .get(this.url)
-      .subscribe((data) => {
-        // console.log(data);
-      });
+  get(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(this.url)
+      .pipe(
+        map(contacts => {
+          return contacts as Contact[];
+        })
+      );
   }
 }
